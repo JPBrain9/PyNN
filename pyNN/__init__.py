1f27e2f3ffa15b6f759b6a6f1b2338efd699a4da
JPBrain9 <julien.panteri.pro@gmail.com>
2021-06-22 17:17:27 +0200

    modification of 22.06.2021

--- pyNN/__init__.py
+++ pyNN/__init__.py
@@ -1,75 +1,50 @@
+# encoding: utf-8
 """
-PyNN (pronounced 'pine') is a Python package for simulator-independent
-specification of neuronal network models.
+Defines a backend-independent, partial implementation of the PyNN API
 
-In other words, you can write the code for a model once, using the PyNN API, and
-then run it without modification on any simulator that PyNN supports.
+Backend simulator modules are not required to use any of the code herein,
+provided they provide the correct interface, but it is suggested that they use
+as much as is consistent with good performance (optimisations may require
+overriding some of the default definitions given here).
 
-To use PyNN, import the particular simulator module you wish to use, e.g.
-    import pyNN.neuron as sim
-all subsequent code in the `sim` namespace will then have the same behaviour
-independent of simulator.
+Utility functions and classes:
+    is_conductance()
+    check_weight()
 
-Functions for simulation set-up and control:
-    setup()
-    run()
-    run_until()
-    reset()
-    end()
-    get_time_step()
-    get_current_time()
-    get_min_delay()
-    get_max_delay()
-    rank()
-    num_processes()
-    list_standard_models()
+Base classes to be sub-classed by individual backends:
+    IDMixin
+    Population
+    PopulationView
+    Assembly
+    Projection
 
-Functions for creating, connecting, modifying and recording from neurons
-(low-level interface):
-    create()
-    connect()
+Function-factories to generate backend-specific API functions:
+    build_reset()
+    build_state_queries()
+    build_create()
+    build_connect()
+    build_record()
+
+Common implementation of API functions:
     set()
-    record()
     initialize()
 
-Classes for creating, connecting, modifying and recording from neurons
-(high-level interface):
-    Population
-    Projection
-    Space
-    Structures: Line, Grid2D, Grid3D, RandomStructure
-    Connectors: AllToAllConnector, OneToOneConnector, FixedProbabilityConnector,
-                DistanceDependentProbabilityConnector, FixedNumberPreConnector,
-                FixedNumberPostConnector, FromListConnector, FromFileConnector,
-                CSAConnector, ArrayConnector, IndexBasedConnector
-    Standard cell types: IF_curr_exp, IF_curr_alpha, IF_cond_exp, IF_cond_alpha,
-                IF_cond_exp_gsfa_grr, IF_facets_hardware1, HH_cond_exp,
-                EIF_cond_alpha_isfa_ista, EIF_cond_exp_isfa_ista,
-                SpikeSourcePoisson, SpikeSourceArray, SpikeSourceInhGamma
-                (not all cell types are available for all simulator backends).
-    Standard synapse types: StaticSynapse, TsodyksMarkramSynapse, STDPMechanism,
-                AdditiveWeightDependence, MultiplicativeWeightDependence,
-                AdditivePotentiationMultiplicativeDepression,
-                GutigWeightDependence, SpikePairRule
-                (not all combinations area available for all simulator backends).
-    Current injection: DCSource, ACSource, StepCurrentSource, NoisyCurrentSource.
-    File types: StandardTextFile, PickleFile, NumpyBinaryFile, HDF5ArrayFile
+Function skeletons to be extended by backends:
+    setup()
+    end()
+    run()
 
-Available simulator modules:
-    nest
-    neuron
-    brian
-
-Other modules:
-    utility
-    random
-    space
+Global constants:
+    DEFAULT_MAX_DELAY
+    DEFAULT_TIMESTEP
+    DEFAULT_MIN_DELAY
 
 :copyright: Copyright 2006-2020 by the PyNN team, see AUTHORS.
 :license: CeCILL, see LICENSE for details.
+
 """
 
-__version__ = '0.10.0.dev'
-__all__ = ["common", "random", "nest", "neuron", "brian", "brian2",
-           "recording", "errors", "space", "descriptions",
-           "standardmodels", "parameters", "core", "serialization"]
+from .populations import IDMixin, BasePopulation, Population, PopulationView, Assembly, is_conductance
+from .projections import Projection, Connection
+from .procedural_api import build_create, build_connect, set, build_record, initialize
+from .control import setup, end, build_run, build_reset, build_state_queries
